27911d0b8bf2567e58f06d22fa29797d67092dce
LilyEvansHogwarts <825196119@qq.com>
2018-02-10 20:58:03 +0800

    maxStack1

--- chapter3/3.7/maxStack1.c
+++ chapter3/3.7/maxStack1.c
@@ -2,3 +2,57 @@
 
 const int stackSize = 256;
 
+class maxStack {
+public:
+    maxStack {
+	stackTop = -1;
+	maxStackItemIndex = -1;
+    }
+
+    void push(int x) {
+	stackTop++;
+	stackItem[stackTop] = x;
+	if(maxStackItemIndex == -1 || x >= stackItem[maxStackItemIndex]) {
+	    linkNextMaxItem[stackTop] = maxStackItemIndex;
+	    maxStackItemIndex = stackTop;
+	} else 
+	    linkNextMaxItem[stackTop] = -1;
+    }
+
+    int pop() {
+	if(stackTop == -1) return INT_MIN;
+	if(stackTop == maxStackItemIndex)
+	    maxStackItemIndex = linkNextMaxItem[stackTop];
+	int res = stackItem[stackTop];
+	stackTop--;
+	return res;
+    }
+
+    int Max() {
+	if(maxStackItemIndex == -1) return INT_MIN;
+	return stackItem[maxStackItemIndex];
+    }
+
+    bool empty() {
+	return stackTop == -1;
+    }
+
+private:
+    int stackTop;
+    int maxStackItemIndex;
+    int stackItem[stackSize];
+    int linkNextMaxItem[stackSize];
+};
+
+int main() {
+    maxStack s;
+    for(int i = 0;i < 10;i++) {
+	int res = rand()%100;
+	s.push(res);
+	cout << res << " " << s.Max() << endl;
+    }
+    cout << endl;
+    for(int i = 0;i < 10;i++) 
+	cout << s.pop() << " " << s.Max() << endl;
+    return 0;
+}
